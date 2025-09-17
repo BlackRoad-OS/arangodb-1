5c2942ed5a16a367e0a98b7c9b7d2c6684972e7d
Julia CP <julia@arangodb.com>
2025-09-17 13:29:37 -0300

    Fix supervised buffer to account memory of stolen buffer in usage scope and make usage scope a member on the stack, not a unique_ptr

--- lib/Basics/SupervisedBuffer.h
+++ lib/Basics/SupervisedBuffer.h
@@ -11,13 +11,23 @@ class SupervisedBuffer : public Buffer<uint8_t> {
   SupervisedBuffer() = delete;
 
   explicit SupervisedBuffer(arangodb::ResourceMonitor& monitor)
-      : _usageScope(std::make_unique<ResourceUsageScope>(monitor, 0)) {
-    _usageScope->increase(this->capacity());
+      : _usageScope{monitor, 0} {
+    _usageScope.increase(this->capacity());
+  }
+
+  uint8_t* stealWithMemoryAccounting(ResourceUsageScope& owningScope) noexcept {
+    owningScope.increase(_usageScope.trackedAndSteal());
+    uint8_t* ptr = Buffer<uint8_t>::steal();
+    _usageScope.increase(this->capacity());
+    return ptr;
   }
 
   uint8_t* steal() noexcept override {
+    TRI_ASSERT(false)
+        << "raw steal() call not permitted in Supervised Buffer, please use "
+           "stealWithMemoryAccounting(ResourceUsageScope& )";
     uint8_t* ptr = Buffer<uint8_t>::steal();
-    _usageScope->revert();
+    _usageScope.revert();
     return ptr;
   }
 
@@ -27,7 +37,7 @@ class SupervisedBuffer : public Buffer<uint8_t> {
     Buffer<uint8_t>::grow(length);
     auto newCapacity = this->capacity();
     if (newCapacity > currentCapacity) {
-      _usageScope->increase(newCapacity - currentCapacity);
+      _usageScope.increase(newCapacity - currentCapacity);
     }
   }
 
@@ -37,11 +47,11 @@ class SupervisedBuffer : public Buffer<uint8_t> {
     auto after = this->capacity();
     // if before > after, means that it has released usage from the heap
     if (before > after) {
-      _usageScope->decrease(before - after);
+      _usageScope.decrease(before - after);
     }
   }
 
-  std::unique_ptr<ResourceUsageScope> _usageScope;
+  ResourceUsageScope _usageScope;
 };
 
 }  // namespace arangodb::velocypack
