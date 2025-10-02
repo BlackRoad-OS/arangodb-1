a0d3c48a5a91db3f8caada1a640b9b8083e6f701
Markus Pfeiffer <markus.pfeiffer+github@arangodb.com>
2025-10-02 14:25:42 +0100

    Remove TraverserCache

--- tests/Aql/Executor/ShortestPathExecutorTest.cpp
+++ tests/Aql/Executor/ShortestPathExecutorTest.cpp
@@ -60,11 +60,10 @@ namespace arangodb {
 namespace tests {
 namespace aql {
 
-class TokenTranslator : public TraverserCache {
+class TokenTranslator {
  public:
   TokenTranslator(Query* query, BaseOptions* opts)
-      : TraverserCache(*query, opts),
-        _edges(11, arangodb::basics::VelocyPackHelper::VPackHash(),
+      : _edges(11, arangodb::basics::VelocyPackHelper::VPackHash(),
                arangodb::basics::VelocyPackHelper::VPackEqual()) {}
   ~TokenTranslator() = default;
 
@@ -235,10 +234,7 @@ class FakePathFinder {
 };
 
 struct TestShortestPathOptions : public ShortestPathOptions {
-  TestShortestPathOptions(Query* query) : ShortestPathOptions(*query) {
-    std::unique_ptr<TraverserCache> cache =
-        std::make_unique<TokenTranslator>(query, this);
-  }
+  TestShortestPathOptions(Query* query) : ShortestPathOptions(*query) {}
 };
 
 using Vertex = GraphNode::InputVertex;
@@ -392,7 +388,6 @@ class ShortestPathExecutorTest : public ::testing::Test {
   TestShortestPathOptions options;
   ShortestPathOptions defaultOptions;
   std::unique_ptr<BaseOptions> dummy;
-  std::unique_ptr<TraverserCache> cache;
   TokenTranslator translator;
 
   RegisterInfos registerInfos;
@@ -418,9 +413,6 @@ class ShortestPathExecutorTest : public ::testing::Test {
         options(fakedQuery.get()),
         defaultOptions(*fakedQuery.get()),
         dummy(std::make_unique<ShortestPathOptions>(defaultOptions)),
-        cache(std::make_unique<TraverserCache>(
-            *fakedQuery.get(),
-            dynamic_cast<ShortestPathOptions*>(&defaultOptions))),
         translator(fakedQuery.get(), dummy.get()),
         registerInfos(parameters._inputRegisters, parameters._outputRegisters,
                       2, 4, {}, {RegIdSet{0, 1}}),
