6d111f6771952e5bd6f8316c7b4a797d0e5ff0e7
Julia CP <julia@arangodb.com>
2025-11-23 21:51:50 -0300

    Fixed signature of deallocateSupervised

--- arangod/Aql/AqlValue.h
+++ arangod/Aql/AqlValue.h
@@ -563,7 +563,8 @@ struct AqlValue final {
   static uint8_t* allocateSupervised(arangodb::ResourceMonitor& rm,
                                      std::uint64_t len);
 
-  static void deallocateSupervised(uint8_t* base, std::uint64_t len) noexcept;
+  static void deallocateSupervised(uint8_t* base, std::uint64_t len,
+                                   arangodb::ResourceMonitor* rm) noexcept;
 
   /// @brief get the ResourceMonitor pointer if this is a VPACK_SUPERVISED_SLICE
   arangodb::ResourceMonitor* getResourceMonitor() const noexcept {
