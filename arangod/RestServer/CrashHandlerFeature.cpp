f8cfcb030ff6d7167a04b16938be1aebd8db1a64
Jure Bajic <jure.bajic94@gmail.com>
2025-12-17 14:28:52 +0100

    Add CrashHandler feature

---
+++ arangod/RestServer/CrashHandlerFeature.cpp
@@ -0,0 +1,85 @@
+////////////////////////////////////////////////////////////////////////////////
+/// DISCLAIMER
+///
+/// Copyright 2014-2024 ArangoDB GmbH, Cologne, Germany
+/// Copyright 2004-2014 triAGENS GmbH, Cologne, Germany
+///
+/// Licensed under the Business Source License 1.1 (the "License");
+/// you may not use this file except in compliance with the License.
+/// You may obtain a copy of the License at
+///
+///     https://github.com/arangodb/arangodb/blob/devel/LICENSE
+///
+/// Unless required by applicable law or agreed to in writing, software
+/// distributed under the License is distributed on an "AS IS" BASIS,
+/// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+/// See the License for the specific language governing permissions and
+/// limitations under the License.
+///
+/// Copyright holder is ArangoDB GmbH, Cologne, Germany
+///
+/// @author Jan Steemann
+////////////////////////////////////////////////////////////////////////////////
+
+#include "RestServer/CrashHandlerFeature.h"
+
+#include "ApplicationFeatures/ApplicationServer.h"
+#include "ApplicationFeatures/GreetingsFeaturePhase.h"
+#include "CrashHandler/CrashHandlerInterface.h"
+#include "ProgramOptions/Parameters.h"
+#include "ProgramOptions/ProgramOptions.h"
+
+using namespace arangodb;
+using namespace arangodb::options;
+
+CrashHandlerFeature::CrashHandlerFeature(Server& server,
+                                         CrashHandlerInterface* crashHandler)
+    : ArangodFeature{server, *this},
+      _crashHandler(crashHandler),
+      _enabled(true) {
+  setOptional(false);
+  // This feature should start very early
+  startsAfter<GreetingsFeaturePhase>();
+}
+
+void CrashHandlerFeature::collectOptions(
+    std::shared_ptr<ProgramOptions> options) {
+  options->addOption(
+      "--crash-handler.enabled",
+      "Enable crash dump logging to write crash information to disk.",
+      new BooleanParameter(&_enabled),
+      arangodb::options::makeDefaultFlags(
+          arangodb::options::Flags::DefaultNoComponents,
+          arangodb::options::Flags::OnCoordinator,
+          arangodb::options::Flags::OnDBServer,
+          arangodb::options::Flags::OnAgent,
+          arangodb::options::Flags::OnSingle));
+}
+
+void CrashHandlerFeature::setDatabaseDirectory(std::string path) {
+  if (_enabled && _crashHandler != nullptr) {
+    _crashHandler->setDatabaseDirectory(std::move(path));
+  }
+}
+
+std::vector<std::string> CrashHandlerFeature::listCrashes() const {
+  if (!_enabled || _crashHandler == nullptr) {
+    return {};
+  }
+  return _crashHandler->listCrashes();
+}
+
+std::unordered_map<std::string, std::string>
+CrashHandlerFeature::getCrashContents(std::string_view crashId) const {
+  if (!_enabled || _crashHandler == nullptr) {
+    return {};
+  }
+  return _crashHandler->getCrashContents(crashId);
+}
+
+bool CrashHandlerFeature::deleteCrash(std::string_view crashId) const {
+  if (!_enabled || _crashHandler == nullptr) {
+    return false;
+  }
+  return _crashHandler->deleteCrash(crashId);
+}
---
+++ arangod/RestServer/CrashHandlerFeature.h
@@ -0,0 +1,76 @@
+////////////////////////////////////////////////////////////////////////////////
+/// DISCLAIMER
+///
+/// Copyright 2014-2024 ArangoDB GmbH, Cologne, Germany
+/// Copyright 2004-2014 triAGENS GmbH, Cologne, Germany
+///
+/// Licensed under the Business Source License 1.1 (the "License");
+/// you may not use this file except in compliance with the License.
+/// You may obtain a copy of the License at
+///
+///     https://github.com/arangodb/arangodb/blob/devel/LICENSE
+///
+/// Unless required by applicable law or agreed to in writing, software
+/// distributed under the License is distributed on an "AS IS" BASIS,
+/// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+/// See the License for the specific language governing permissions and
+/// limitations under the License.
+///
+/// Copyright holder is ArangoDB GmbH, Cologne, Germany
+///
+/// @author Jan Steemann
+////////////////////////////////////////////////////////////////////////////////
+
+#pragma once
+
+#include <string>
+#include <string_view>
+#include <unordered_map>
+#include <vector>
+
+#include "RestServer/arangod.h"
+
+namespace arangodb {
+
+class CrashHandlerInterface;
+
+/// @brief Feature to control crash dump logging to the database directory.
+/// The CrashHandler itself always runs for crash handling, but this feature
+/// controls whether additional crash information is written to disk.
+class CrashHandlerFeature final : public ArangodFeature {
+ public:
+  static constexpr std::string_view name() noexcept { return "CrashHandler"; }
+
+  explicit CrashHandlerFeature(Server& server,
+                               CrashHandlerInterface* crashHandler);
+
+  void collectOptions(std::shared_ptr<options::ProgramOptions>) override final;
+
+  /// @brief returns true if crash dump logging is enabled
+  bool isEnabled() const noexcept { return _enabled; }
+
+  /// @brief returns the crash handler interface pointer
+  CrashHandlerInterface* crashHandler() const noexcept { return _crashHandler; }
+
+  /// @brief sets the database directory for crash dumps
+  void setDatabaseDirectory(std::string path);
+
+  /// @brief lists all crash directories (returns UUIDs)
+  std::vector<std::string> listCrashes() const;
+
+  /// @brief gets the contents of a specific crash directory
+  std::unordered_map<std::string, std::string> getCrashContents(
+      std::string_view crashId) const;
+
+  /// @brief deletes a specific crash directory
+  bool deleteCrash(std::string_view crashId) const;
+
+ private:
+  /// @brief pointer to the CrashHandler interface (not owned)
+  CrashHandlerInterface* _crashHandler;
+
+  /// @brief whether crash dump logging is enabled
+  bool _enabled;
+};
+
+}  // namespace arangodb
