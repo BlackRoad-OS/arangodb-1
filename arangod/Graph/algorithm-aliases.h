b712f6fc4a19943677145c3adad7067c94e242ec
Julia Volmer <julia.volmer@arangodb.com>
2026-01-07 20:09:01 +0100

    Make BFS work

--- arangod/Graph/algorithm-aliases.h
+++ arangod/Graph/algorithm-aliases.h
@@ -30,7 +30,7 @@
 #include "Graph/Enumerators/YenEnumerator.h"
 
 #include "Graph/Queues/BatchedLifoQueue.h"
-// #include "Graph/Queues/BatchedFifoQueue.h"
+#include "Graph/Queues/BatchedFifoQueue.h"
 #include "Graph/Queues/FifoQueue.h"
 #include "Graph/Queues/LifoQueue.h"
 #include "Graph/Queues/WeightedQueue.h"
@@ -112,11 +112,12 @@ struct BFSConfiguration {
   // smart traversal queue on coordinator needs to be non-batched
   // for that, SmartGraphProvider fns addExpansionIterator and expandToNextBatch
   // need to be implemented
-  using Queue = FifoQueue<Step>;
-  // using Queue = typename std::conditional<
-  //     std::is_same_v<ProviderType,
-  //                    enterprise::SmartGraphProvider<ClusterProviderStep>>,
-  //     FifoQueue<Step>, BatchedFifoQueue<Step>>::type;
+  // using Queue = FifoQueue<Step>;
+  using Queue = typename std::conditional<
+      std::is_same_v<ProviderType,
+                     enterprise::SmartGraphProvider<ClusterProviderStep>>,
+      FifoQueue<Step>,
+      BatchedFifoQueue<Step, typename ProviderType::NeighbourProvider>>::type;
   using Store = PathStore<Step>;
   using Validator =
       PathValidator<Provider, Store, vertexUniqueness, edgeUniqueness>;
